bfbc69af37dbbfd1be320ca869322808b12549df
estherpunter <estherpunter5@hotmail.com>
2023-06-23 17:28:22 +0200

    Opdracht 1 voltooid

--- scripts/main.js
+++ scripts/main.js
@@ -72,3 +72,18 @@ const departments = {
 }
 
 console.log(departments);
+
+// Opdracht 1a.
+console.log("De afdeling Sales heeft " + departments.sales.numberOfEmployees + " medewerkers");
+
+// Opdracht 1b.
+console.log("Marketing is een leuke afdeling om te werken. " + departments.marketing.description);
+
+// Opdracht 1c.
+console.log("De afdeling Customer Service heeft " + departments["customer-service"].numberOfEmployees + " medewerkers");
+
+// Opdracht 1d.
+console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager. " + departments.sales.jobs[1].description);
+
+
+//Opdracht 2
